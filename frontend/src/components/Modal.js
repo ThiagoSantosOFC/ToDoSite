db7c51042dd1fb1182163d527d9ac5160867a5f1
Thiago <74683757+ThiagoShow@users.noreply.github.com>
2022-06-16 09:32:26 +0200

    fix

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,41 +1,41 @@
-import React, { Component } from 'react';
-
-import{
-    Button,
-    Modal,
-    ModalHeader,
-    ModalBody,
-    ModalFooter,
-    Form,
-    FormGroup,
-    Label,
-    Input,
+import React, { Component } from "react";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+  FormGroup,
+  Input,
+  Label,
 } from "reactstrap";
 
 export default class CustomModal extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            activeItem: this.props.activeItem,
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeItem: this.props.activeItem,
+    };
+  }
+
+  handleChange = (e) => {
+    let { name, value } = e.target;
+
+    if (e.target.type === "checkbox") {
+      value = e.target.checked;
     }
 
-handleChange = (e) => {
-    let {name, value} = e.target;
-    
-    if (e.target.type === 'checkbox') {
-        value = e.target.checked;
-    }
+    const activeItem = { ...this.state.activeItem, [name]: value };
 
-    const activeItem = {...this.state.activeItem, [name]: value};
+    this.setState({ activeItem });
+  };
 
-    this.setState({activeItem});
-};
+  render() {
+    const { toggle, onSave } = this.props;
 
-render(){
-    const {toggle, onSave} = this.props;
-
-    return ( <Modal isOpen={true} toggle={toggle}>
+    return (
+      <Modal isOpen={true} toggle={toggle}>
         <ModalHeader toggle={toggle}>Todo Item</ModalHeader>
         <ModalBody>
           <Form>
@@ -82,6 +82,7 @@ render(){
             Save
           </Button>
         </ModalFooter>
-      </Modal>);
- };
-};
+      </Modal>
+    );
+  }
+}
